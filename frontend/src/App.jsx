01bf3bd387cd6d9a97c4fb2778d979afd5d85856
Divyanshu Tiwari <dtiwari@ar.iitr.ac.in>
2023-09-27 11:06:57 +0530

    Initial frontend UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const startScraping = useCallback(() => {}, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<h1>Youtube scraper</h1>
+			<label htmlFor="keywords">Keywords: </label>
+			<br />
+			<input id="keywords" placeholder="coding, programming"></input>
+
+			<br />
+			<label htmlFor="keywords">Min. Subscribers</label>
+			<br />
+			<input id="keywords" placeholder="1000"></input>
+
+			<br />
+			<label htmlFor="keywords">Max. Subscribers</label>
+			<br />
+			<input id="keywords" placeholder="1000"></input>
+
+			<br />
+			<label htmlFor="keywords">Max. channels per keyword</label>
+			<br />
+			<input id="keywords" placeholder="1000000"></input>
+
+			<br />
+			<button onClick={startScraping}>Scrape</button>
+		</>
+	);
 }
 
-export default App
+export default App;
